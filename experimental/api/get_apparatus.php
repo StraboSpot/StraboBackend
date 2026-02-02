a86237311a853bb25334eb6db54712a9b17164aa
Jason <jason@jasonash.com>
2026-02-02 17:45:39 -0600

    [Security] Fix SQL injection in apparatus repository endpoints
    Convert direct string interpolation to prepared statements:
    - get_apparatus.php: Use get_row_prepared()
    - get_facility.php: Use get_row_prepared()
    - get_apprepo_apparatus.php: Use get_row_prepared()
    - get_apprepo_facility.php: Use get_row_prepared()
    - get_apparatus_list.php: Use get_results_prepared()

--- experimental/api/get_apparatus.php
+++ experimental/api/get_apparatus.php
@@ -42,7 +42,7 @@ if ($id == 0) {
 }
 
 // Query apparatus
-$row = $db->get_row("SELECT * FROM apprepo.apparatus WHERE pkey = $id");
+$row = $db->get_row_prepared("SELECT * FROM apprepo.apparatus WHERE pkey = $1", array($id));
 
 if (empty($row->pkey)) {
     $error = new stdClass();
