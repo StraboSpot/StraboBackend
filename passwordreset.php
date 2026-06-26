df50ac6c8ed16545c45aa49ab23ae7c514f0b62d
Jason <jason@jasonash.com>
2026-06-26 13:40:18 -0500

    [Refactor] Update password reset page to new look and feel

--- passwordreset.php
+++ passwordreset.php
@@ -41,39 +41,60 @@ if($count > 0){
 			//update password - SECURE: Using prepared statement
 			$db->prepare_query("UPDATE users SET password=crypt($1, gen_salt('md5')) WHERE hash=$2", array($password, $hash));
 
-			include("includes/header.php");
+			include("includes/mheader.php");
 			?>
+			<!-- Main -->
+				<div id="main" class="wrapper style1">
+					<div class="container">
 
-				<h1>Success</h1><br><br>
+						<header class="major">
+							<h2>Success!</h2>
+						</header>
 				Password has been reset.
+					<div class="bottomSpacer"></div>
 
+					</div>
+				</div>
 			<?php
-			include("includes/footer.php");
+			include("includes/mfooter.php");
 			exit();
 		}
 	}
 
 	if($error!=""){
-		$error="<div style=\"color:#FF0000;padding:10px;\">$error</div>";
+		$error="<div style=\"color:#e44c65;padding:10px;\">$error</div>";
 	}
 
-	include("includes/header.php");
+	include("includes/mheader.php");
 
 	?>
+			<!-- Main -->
+				<div id="main" class="wrapper style1">
+					<div class="container">
 
-		<h1>Password Reset</h1><br>
+						<header class="major">
+							<h2>Password Reset</h2>
+						</header>
 		<?php echo $error?>
 		<form method="POST">
-		<table>
-			<tr><td>New Password: </td><td><input type="password" name="password" value="<?php echo $password?>"></td></tr>
-			<tr><td>Confirm New Password: </td><td><input type="password" name="passwordconfirm" value="<?php echo $passwordconfirm?>"></td></tr>
-			<tr><td colspan="2"><div align="center"><input type="submit" name="submit" value="Submit"></div></td></tr>
-		</table>
+			<div class="row gtr-uniform gtr-50">
+			<div class="col-12"><h3>New Password:</h3></div>
+			<div class="col-12"><input type="password" name="password" value="<?php echo $password?>"></div>
+			<div class="col-12"><h3>Confirm New Password:</h3></div>
+			<div class="col-12"><input type="password" name="passwordconfirm" value="<?php echo $passwordconfirm?>"></div>
+			<div class="col-12"><input class="primary" type="submit" name="submit" value="Submit"></div>
+			</div>
 		<input type="hidden" name="hash" value="<?php echo $hash?>">
 		</form>
+
+					<div class="bottomSpacer"></div>
+
+					</div>
+				</div>
+
 	<?php
 
-	include("includes/footer.php");
+	include("includes/mfooter.php");
 
 }
 
